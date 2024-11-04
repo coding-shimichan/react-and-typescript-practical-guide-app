ed6bf8cff82bfdf67f7a17daf3ca90a3b46c4c59
tmsby <kafa.thud.zaharo@gmail.com>
2024-11-04 16:59:44 +0900

    Props with children

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,9 @@ import CourseGoal from "./components/CourseGoal";
 export default function App() {
   return (
     <main>
-      <CourseGoal
-        title="Learn React + TS"
-        description="Learning it from ground up"
-      />
+      <CourseGoal title="Learn React + TS">
+        <p>Learning it from ground!</p>
+      </CourseGoal>
     </main>
   );
 }
