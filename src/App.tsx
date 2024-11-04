b3ea0e9846961a419b0dce727c5d5158ceca7cf3
tmsby <kafa.thud.zaharo@gmail.com>
2024-11-04 14:00:38 +0900

    Add props for the CourseGoal component

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,10 @@ import CourseGoal from "./components/CourseGoal";
 export default function App() {
   return (
     <main>
-      <CourseGoal />
+      <CourseGoal
+        title="Learn React + TS"
+        description="Learning it from ground up"
+      />
     </main>
   );
 }
