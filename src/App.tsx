961bb68569cdd3a14852d2354253be90c26acb1e
tmsby <kafa.thud.zaharo@gmail.com>
2024-11-06 22:26:02 +0900

    Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import CourseGoal from "./components/CourseGoal";
+import goalsImg from "./assets/goals.jpg";
+import Header from "./components/Header";
 
 export default function App() {
   return (
     <main>
+      <Header image={{ src: goalsImg, alt: "A list of goals" }}>
+        <h1>Your Course Goals</h1>
+      </Header>
       <CourseGoal title="Learn React + TS">
         <p>Learning it from ground!</p>
       </CourseGoal>
